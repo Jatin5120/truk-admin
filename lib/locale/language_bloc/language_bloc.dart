93453c0330b66d6164815ec785a9a5967aa7eaf7
Jatin <getjatin5120@gmail.com>
2021-10-10 11:16:14 +0530

    Add Repo

--- lib/locale/language_bloc/language_bloc.dart
+++ lib/locale/language_bloc/language_bloc.dart
@@ -22,26 +22,31 @@ class LanguageBloc extends Bloc<LanguageEvent, Language> {
   Stream<Language> _mapLanguageLoadStartedToState() async* {
     final sharedPrefService = SharedPref();
     final defaultLanguage = SharedPref.en;
-    Locale locale = await sharedPrefService.getLocale() ?? defaultLanguage;
+    Locale locale = await sharedPrefService.getLocale();
 
     yield Language(locale);
   }
 
-  Stream<Language> _mapLanguageSelectedToState(Language selectedLanguage) async* {
+  Stream<Language> _mapLanguageSelectedToState(
+      Language selectedLanguage) async* {
     final sharedPrefService = SharedPref();
-    final defaultLanguage = await sharedPrefService.getLocale() ?? SharedPref.en;
+    final defaultLanguage = await sharedPrefService.getLocale();
 
-    if (selectedLanguage.locale == SharedPref.hi && defaultLanguage != SharedPref.hi) {
+    if (selectedLanguage.locale == SharedPref.hi &&
+        defaultLanguage != SharedPref.hi) {
       yield* _loadLanguage(sharedPrefService, 'hi', 'IN');
-    } else if (selectedLanguage.locale == SharedPref.en && defaultLanguage != SharedPref.en) {
+    } else if (selectedLanguage.locale == SharedPref.en &&
+        defaultLanguage != SharedPref.en) {
       yield* _loadLanguage(sharedPrefService, 'en', 'US');
-    } else if (selectedLanguage.locale == SharedPref.te && defaultLanguage != SharedPref.te) {
+    } else if (selectedLanguage.locale == SharedPref.te &&
+        defaultLanguage != SharedPref.te) {
       yield* _loadLanguage(sharedPrefService, 'te', 'IN');
     }
   }
 
   /// This method is added to reduce code repetition.
-  Stream<Language> _loadLanguage(SharedPref sharedPref, String languageCode, String countryCode) async* {
+  Stream<Language> _loadLanguage(
+      SharedPref sharedPref, String languageCode, String countryCode) async* {
     final locale = Locale(languageCode, countryCode);
     await sharedPref.setLocale(locale.languageCode);
     yield Language(locale);
